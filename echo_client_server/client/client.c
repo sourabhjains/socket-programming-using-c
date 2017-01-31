e58fb4a7e8c55b5df0f27432d6f39a313618d375
sourabh jain <sourabhjains>
2017-02-01 00:43:55 +0530

    Description Added

--- echo_client_server/client/client.c
+++ echo_client_server/client/client.c
@@ -1,38 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<string.h>
-#include<errno.h>
-#include <arpa/inet.h>
 
+// To use basic networking functions like:- socket, connect, send, recv
+#include<sys/socket.h>
+
+// To get the cause of an error
+#include<errno.h>
+
+// To perform operations on string like:- strlen
+#include<string.h>
+
+// To perform conversion of data like:- network to host, ascii to host
+#include<arpa/inet.h>
+
+// To handle the file descriptors
+#include<unistd.h>
 
 #define ERROR -1
-#define BUFFER 1024
+#define BUFFER 512
 
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_in remoteServer;
-	int sock;
-	char input[BUFFER];
+	struct sockaddr_in server;	// Store server socket information
+	int sock;			// Server socket descriptor
+	char input[BUFFER];		
 	char output[BUFFER];
 	int len;
 	
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR)
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR)	// Create socket by giving address type, transmission mechanism, protocol
 	{
 		perror("Socket :");		
 		exit(-1);
 	}
+							// Assigning the values to the server socket
+	server.sin_family = AF_INET;			// Address type (AF_INET -> ipv4)
+	server.sin_port = htons(atoi(argv[2]));		// Port number (atoi - ascii to integer , htons - integer to network byte order)
+	server.sin_addr.s_addr = inet_addr(argv[1]);	// Interface
+	bzero(&server.sin_zero, 8);
 	
-	remoteServer.sin_family = AF_INET;
-	remoteServer.sin_port = htons(atoi(argv[2]));
-	remoteServer.sin_addr.s_addr = inet_addr(argv[1]);
-	bzero(&remoteServer.sin_zero, 8);
-	
-	if((connect(sock, (struct sockaddr *)&remoteServer, sizeof(struct sockaddr_in))) == ERROR)
+	if((connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_in))) == ERROR)  // Connects the socket descriptor and server addr
 	{
 		perror("Connect :");
 		exit(-1);
@@ -40,13 +47,13 @@ int main(int argc, char **argv)
 
 	while(1)
 	{
-		fgets(input, BUFFER, stdin);
-		send(sock, input, strlen(input), 0);
-		len = recv(sock, output, BUFFER, 0);
+		fgets(input, BUFFER, stdin);		// Take input stirng from user
+		send(sock, input, strlen(input), 0);	// Write the content of input to sock socket descriptor and send it
+		len = recv(sock, output, BUFFER, 0);	// Copy the content of file descriptor to output buffer
 		output[len] = '\0';		
 		printf("%s", output);
 	}
 	
-	close(sock);
+	close(sock);					// Close the server descriptor
 }
 
--- echo_client_server/server/server.c
+++ echo_client_server/server/server.c
@@ -1,58 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<sys/types.h>
+
+// To use basic networking functions like:- socket, listen, bind, accept, send, recv
 #include<sys/socket.h>
-#include<netinet/in.h>
+
+// To get the cause of an error
 #include<errno.h>
-#include<string.h>
+
+// To perform conversion of data like:- network to host, ascii to host
 #include<arpa/inet.h>
+
+// To handle the file descriptors
 #include<unistd.h>
-#include <netinet/in.h>
-#include <unistd.h>
 
 #define ERROR -1
-#define MAX_CLIENTS 2
-#define MAX_DATA 1024
+#define DATA_LENGTH 512
 
 int main(int argc, char** argv)
 {
-	struct sockaddr_in server;
-	struct sockaddr_in client;
-	int sock;
-	int new;
+	struct sockaddr_in server;     // Store server socket information
+	struct sockaddr_in client;     // Store client socket information
+	int sock;		       // Server socket descriptor
+	int new;		       // Client socket descriptor
 	socklen_t sockaddr_len = sizeof(struct sockaddr_in);
 	int data_len;
-	char data[MAX_DATA];
+	char data[DATA_LENGTH];           // Buffer to hold data
 
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR)
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR)     // Creating socket by giving address type, transmission mechanism, protocol 
 	{
-		perror("Server socket : ");
+		perror("Server socket : ");			  
 		exit(-1);
 	}
-	
-	server.sin_family = AF_INET;
-	server.sin_port = htons(atoi(argv[1]));
-	server.sin_addr.s_addr  = INADDR_ANY;
+							// Assigning the values to the server socket
+	server.sin_family = AF_INET;			// Address type (AF_INET -> ipv4)
+	server.sin_port = htons(atoi(argv[1]));		// Port number (atoi - ascii to integer , htons - integer to network byte order)
+	server.sin_addr.s_addr  = INADDR_ANY;		// Interface 
 	bzero(&server.sin_zero, 8);
 	
-	if((bind(sock, (struct sockaddr *)&server, sockaddr_len)) == ERROR)
+	if((bind(sock, (struct sockaddr *)&server, sockaddr_len)) == ERROR)     // Binding the server socket to a scoket descriptor
 	{
 		perror("Bind : ");
 		exit(-1);
 	}
 
-	if((listen(sock, MAX_CLIENTS)) == ERROR)
-	{
+	if((listen(sock, 1)) == ERROR)						// Putting socket on listening mode
+	{		
 		perror("Listen :");
 		exit(-1);
 	}
 
 	while(1)
 	{
-		new = accept(sock, (struct sockaddr *)&client, &sockaddr_len);
-		printf("sock :%d  \n new :%d\n",sock, new);
-		if(new == ERROR)
-		{
+		new = accept(sock, (struct sockaddr *)&client, &sockaddr_len);	// Accepts the first pending connection in the queue
+		if(new == ERROR)						// Accept method also set the address and port number-
+		{								// to client socket descriptor
 			perror("accept :");
 			exit(-1);		
 		}
@@ -61,18 +62,18 @@ int main(int argc, char** argv)
 		data_len = 1;
 		while(data_len)
 		{
-			data_len = recv(new, data, MAX_DATA, 0);
-			if(data_len)
-			{
-				send(new, data, data_len, 0);
+			data_len = recv(new, data, DATA_LENGTH, 0);		// Reading the data from client socket descriptor
+			if(data_len)	
+			{							// new is client socket descriptor
+				send(new, data, data_len, 0);			// Send the same data back to client
 				data[data_len] = '\0';
 				printf("Sent message : %s", data);		
 			}
 		}
 		printf("Client Disconnected.\n");
-		close(new);
+		close(new);							// Closing  client file descriptor
 	}
-	
+	close(sock);								// Closing server file descriptor
 	
 }
 
